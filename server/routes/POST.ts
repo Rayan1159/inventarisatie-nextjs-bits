ebe36e0af486772853087755fe0689e9ec85491d
rayan1159 <baskruithof999@gmail.com>
2024-10-30 12:15:43 +0100

    code cleanups

--- server/routes/POST.ts
+++ server/routes/POST.ts
@@ -10,40 +10,6 @@ const routerPost = Router();
 User.initModel(sequelize)
 Inventory.initModel(sequelize);
 
-routerPost.post('/user/login', async (req: Request, res: Response, next: NextFunction) => {
-    const {username, password} = req.body;  
-    if (!username || !password) return res.status(400).json({
-        error: 'Username and password are required'
-    });
-
-    const loggedIn = await User.login(username, password);
-    if (loggedIn) {
-        const {username, session_id} = await User.getUser(req.body.username) as User;
-        if (!session_id) await User.createSessionIdentifier(username as string)
-        res.status(200).json({
-            loggedIn: true,
-            userObj: {
-                username,
-                session_id : session_id ? session_id : "error when setting session_id"
-            }
-        })
-    } else {
-        res.status(401).json({
-            loggedIn: false
-        });
-    }
-})
-
-routerPost.post('/user/exists', async (req: Request, res: Response, next: NextFunction) => {
-    const {username} = req.body;
-    const exists = await User.findUser(username);
-
-    res.status(200).json({
-        exists: !!exists
-    })
-})
-
-
 routerPost.post('/entry/create', async (req, res, _next) => {
     const permissionInt = await User.getPermissionLevel(req.body.username) as number;
     if (!permissionInt) {
